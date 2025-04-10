8aa7e91cb6b4c690f169153b8c92d2bbb2e26262
Jack Hawkins <jack.m.hawkins@vanderbilt.edu>
2025-04-10 11:25:29 -0500

    first three tests pass

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,36 +1,126 @@
 ﻿
 //create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+List<Product> products = new List<Product>()
+{
+    new Product()
+    {
+        Name = "A Light in the Attic",
+        Price = 20.00M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "Sousaphone",
+        Price = 2996.00M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Leaves of Grass",
+        Price = 15.00M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "French Horn",
+        Price = 1325.50M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "The Raven",
+        Price = 18.75M,
+        ProductTypeId = 2
+    }
+};
 
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
+List<ProductType> productTypes = new List<ProductType>()
+{
+    new ProductType()
+    {
+        Title = "Brass",
+        Id = 1
+    },
+    new ProductType()
+    {
+        Title = "Poem",
+        Id = 2
+    }
+};
 
 //put your greeting here
+string greeting = @"Welcome to Brass & Poem
+Today's Special Deal is 'The Raven' for only $18.75";
+Console.WriteLine(greeting);
 
 //implement your loop here
-
-void DisplayMenu()
+string choice = null;
+while (choice != "5")
 {
-    throw new NotImplementedException();
-}
+    DisplayMenu();
+    choice = Console.ReadLine();
+    if (choice == "1")
+    {
+        DisplayAllProducts(products, productTypes);
+    }
+    else if (choice == "2")
+    {
+        DeleteProduct(products, productTypes);
+    }
+    else if (choice == "3")
+    {
+        AddProduct(products, productTypes);
+    }
+    else if (choice == "4")
+    {
+        UpdateProduct(products, productTypes);
+    }
+    else if (choice == "5")
+    {
+        Console.WriteLine("Farewell!");
+    }
 
-void DisplayAllProducts(List<Product> products, List<ProductType> productTypes)
-{
-    throw new NotImplementedException();
-}
+    void DisplayMenu()
+    {
+        Console.WriteLine(@"1. Display all products
+2. Delete a product
+3. Add a new product
+4. Update product properties
+5. Exit");
+    }
 
-void DeleteProduct(List<Product> products, List<ProductType> productTypes)
-{
-    throw new NotImplementedException();
-}
 
-void AddProduct(List<Product> products, List<ProductType> productTypes)
-{
-    throw new NotImplementedException();
-}
+    void DisplayAllProducts(List<Product> products, List<ProductType> productTypes)
+    {
+        for (int i = 0; i < products.Count; i++)
+        {
+            Product product = products[i]; //set to current product we're cycling thru
+            string productTypeTitle = productTypes.First(pType => pType.Id == product.ProductTypeId).Title; //get our type
+            Console.WriteLine($"{i + 1}. {product.Name} - ${product.Price} - {productTypeTitle}");
+        }
+    }
 
-void UpdateProduct(List<Product> products, List<ProductType> productTypes)
-{
-    throw new NotImplementedException();
-}
+    void DeleteProduct(List<Product> products, List<ProductType> productTypes)
+    {
+        DisplayAllProducts(products, productTypes);
+        Console.WriteLine("Please enter the number of the product you want to delete:");
+        string input = Console.ReadLine();
+        int index = int.Parse(input) - 1;
+        products.RemoveAt(index); //little confused at difference between this and a plain Remove - maybe ask question
+    }
 
+    void AddProduct(List<Product> products, List<ProductType> productTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+    void UpdateProduct(List<Product> products, List<ProductType> productTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
 // don't move or change this!
 public partial class Program { }
